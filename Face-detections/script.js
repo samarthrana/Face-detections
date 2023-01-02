1eed4c85f51f53c73d575a8b904afbaa3d59ce08
Samarth Rana <76391979+samarthrana@users.noreply.github.com>
2023-01-02 12:31:35 +0530

    Add files via upload

---
+++ Face-detections/script.js
@@ -0,0 +1,34 @@
+const video = document.getElementById("video");
+
+const startVideo = () =>{
+    navigator.getUserMedia(
+        {video: {}},
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/weights'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/weights'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/weights'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/weights'),
+]).then(startVideo)
+
+
+startVideo()
+
+video.addEventListener('play',()=>{
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {width: video.width, height:video.height}
+    faceapi.matchDimensions(canvas,displaySize)
+    setInterval(async () =>{
+        const detections = await faceapi.detectAllFaces(video,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections,displaySize)
+        canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
+        faceapi.draw.drawDetections(canvas,resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas,resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas,resizedDetections)
+    },100)
+})
